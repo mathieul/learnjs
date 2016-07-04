c943debef452056a23951bdff33f674b6f674348
Mathieu Lajugie <mathieu@caring.com>
2016-07-04 11:30:54 -0700

    customize config a bit

--- services/lib/deploy.js
+++ services/lib/deploy.js
@@ -1,13 +1,13 @@
 var AWS = require('aws-sdk');
 var fs = require('fs');
 
-AWS.config.region = 'us-east-1'
+AWS.config.region = 'us-west-2'
 var lambda = new AWS.Lambda({apiVersion: '2015-03-31'});
 var params = {
   FunctionName: 'learnjs_popularAnswers',
   ZipFile: fs.readFileSync('archive.zip')
 };
-lambda.updateFunctionCode(params, function(err, data) { 
+lambda.updateFunctionCode(params, function(err, data) {
   if (err) console.log(err, err.stack); // an error occurred
   else     console.log(data);           // successful response
 });
